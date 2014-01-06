8c43fda05a3d36ba0905eb2c5f1f1bdc474ae2fd
Thomas Chapeaux <chapeauxthomas@gmail.com>
2014-01-06 15:10:53 +0100

    Given correct extension to hydra bash files

---
+++ jobFile_mainSimuComp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#PBS -l nodes=8:ppn=2
+#PBS -l walltime=20:00:00
+#PBS -l mem=1Gb
+#PBS -o stdout.txt
+#PBS -e stderr.txt
+#PBS -N mainSimuComp
+
+echo "Running job on $HOST - " `date`
+
+cd $HOME/Repositories/py-rt-simulator
+module load python/3.4.0a1
+#PATH=$PATH:$HOME/glpk/bin
+echo `pwd`
+python3 $HOME/Repositories/py-rt-simulator/mainSimuComp.py
+
+echo "Done"
