e87f0ae2d06914860a90c31a95584b8a0cda3d16
Thomas Chapeaux <chapeauxthomas@gmail.com>
2014-01-07 13:57:41 +0100

    changed my mind about naming. switched test output to verbosity 2

--- runtests.py
+++ runtests.py
@@ -1,13 +1,13 @@
 import unittest
 
-from test.testSimulator import TestSimulator
+from test.testSimulator import TestSimulatorWithKnownSystems
 from test.testMyAlgebra import TestMyAlgebra
 from test.testAlgorithms import TestAlgorithms
 from test.testFindFPDIT import TestFindFPDIT
 from test.testTask import TestTask
 
 if __name__ == '__main__':
-    tests = [TestSimulator, TestMyAlgebra, TestAlgorithms, TestFindFPDIT, TestTask]
+    tests = [TestSimulatorWithKnownSystems, TestMyAlgebra, TestAlgorithms, TestFindFPDIT, TestTask]
     for test in tests:
         suite = unittest.TestLoader().loadTestsFromTestCase(test)
-        unittest.TextTestRunner(verbosity=1).run(suite)
+        unittest.TextTestRunner(verbosity=2).run(suite)
