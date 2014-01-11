78def432fe0642d7707cbd64110ba028ba0e0cde
Thomas Chapeaux <chapeauxthomas@gmail.com>
2014-01-11 18:29:32 +0100

    Removed unneeded dependency

--- runtests.py
+++ runtests.py
@@ -3,7 +3,6 @@ import unittest
 from test.testSimulator import TestSimulator, TestSimulatorWithKnownSystems
 from test.testMyAlgebra import TestMyAlgebra
 from test.testAlgorithms import TestAlgorithms
-from test.testFindFPDIT import TestFindFPDIT
 from test.testTask import TestTask
 
 if __name__ == '__main__':
