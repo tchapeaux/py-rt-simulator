c6c316f892f2e0729cb4681ddf9cc6e6866c7395
Thomas Chapeaux <chapeauxthomas@gmail.com>
2014-01-13 18:33:15 +0100

    more ressources

--- jobFile_mainSimuComp.sh
+++ jobFile_mainSimuComp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
 #PBS -l nodes=8:ppn=2
-#PBS -l walltime=3:00:00
-#PBS -l mem=1gb
+#PBS -l walltime=12:00:00
+#PBS -l mem=16gb
 #PBS -o stdout.txt
 #PBS -e stderr.txt
 #PBS -N mainSimuComp
 
 cd $WORKDIR/py-rt-simulator
+rm stderr.txt stdout.txt
 module load python/3.4.0a1
-python3 mainSimuComp.py -p $HOME/py-rt-SimuComp/results.pickle -n 100000 -sched1 PTEDF -sched2 EDF
+python3 mainSimuComp.py -p $HOME/py-rt-SimuComp/results.pickle -n 100000 -sched1 PMImp -sched2 EDF
