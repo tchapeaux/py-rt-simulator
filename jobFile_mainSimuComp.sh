1312e59b2fe02cf2f8b4693f5c507a1a9d19cdb4
Thomas Chapeaux <chapeauxthomas@gmail.com>
2014-01-18 01:25:36 +0100

    MOAR RESSOURCES

--- jobFile_mainSimuComp.sh
+++ jobFile_mainSimuComp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#PBS -l nodes=8:ppn=2
-#PBS -l walltime=12:00:00
-#PBS -l mem=16gb
+#PBS -l nodes=3:ppn=12
+#PBS -l walltime=24:00:00
+#PBS -l mem=32gb
 #PBS -o stdout.txt
 #PBS -e stderr.txt
 #PBS -N mainSimuComp
@@ -12,4 +12,4 @@ rm -f stdout.txt
 rm -f mainSimuComp_log.txt
 rm -f mainSimuComp_results.pickle
 module load python/3.4.0a1
-python3 mainSimuComp.py -p mainSimuComp_results.pickle -n 10 -sched1 PMImp -sched2 LLF -writeFail 1
+python3 mainSimuComp.py -p mainSimuComp_results.pickle -n 10000 -sched1 PMImp -sched2 Meumeu -writeFail 1
